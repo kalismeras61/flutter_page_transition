7134d03c99be297add12580e82a30697f4e34744
unknown <yasinilhan61@gmail.com>
2018-12-14 16:12:23 +0300

    add new transition

---
+++ example/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Page Transition'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Left Slide Second Page'),
+              onPressed: () {
+                Navigator.push(context, PageTransition(type: 'fade', child: SecondPage()));
+              },
+            ),
+            RaisedButton(
+              child: Text('Left To Right Slide Second Page'),
+              onPressed: () {
+                Navigator.push(context, PageTransition(type:'leftToRight', child: SecondPage()));
+              },
+            ),
+            RaisedButton(
+              child: Text('Size Slide Second Page'),
+              onPressed: () {
+                Navigator.push(context, PageTransition(curve: Curves.bounceOut, type:'size', child: SecondPage()));
+              },
+            ),
+
+            RaisedButton(
+              child: Text('Transform Slide Second Page'),
+              onPressed: () {
+                Navigator.push(context, PageTransition(curve: Curves.bounceOut, type:'transform', child: SecondPage()));
+              },
+            ),
+            RaisedButton(
+              child: Text('Scale Slide Second Page'),
+              onPressed: () {
+                Navigator.push(context, PageTransition(curve: Curves.bounceOut, type:'scale', child: SecondPage()));
+              },
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Transition SecondPage'),
+      ),
+      body: Center(
+        child: Text('Second Page'),
+      ),
+    );
+  }
+}
--- lib/page_transition.dart
+++ lib/page_transition.dart
@@ -2,141 +2,114 @@ library page_transition;
 
 import 'package:flutter/material.dart';
 
-// usage for this route
-//  Navigator.push(context, AppSlideRoute(builder: (context) => new SecondPage(),),);
-class FadeSlideRoute<T> extends PageRouteBuilder<T> {
+class PageTransition<T> extends PageRouteBuilder<T> {
   final Widget child;
+  final String type;
+  final Curve curve ;
 
-  FadeSlideRoute({this.child})
+  PageTransition({Key key, @required this.child, @required this.type, this.curve = Curves.linear})
       : super(pageBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          return child;
-        }, transitionsBuilder: (BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child) {
-          return new FadeTransition(opacity: animation, child: child);
-        });
-}
+      Animation<double> secondaryAnimation) {
+    return child;
+  }, transitionsBuilder: (BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    switch (type) {
+      case 'fade':
+        return FadeTransition(opacity: animation, child: child);
+        break;
+      case 'rightToLeft':
+        return SlideTransition(
+          transformHitTests: true,
+          position: new Tween<Offset>(
+            begin: const Offset(1.0, 0.0),
+            end: Offset.zero,
+          ).animate(animation),
+          child: new SlideTransition(
+            position: new Tween<Offset>(
+              begin: Offset.zero,
+              end: const Offset(1.0, 0.0),
+            ).animate(secondaryAnimation),
+            child: child,
+          ),
+        );
+        break;
+      case 'leftToRight':
+        return SlideTransition(
+          transformHitTests: true,
+          position: Tween<Offset>(
+            begin: const Offset(-1.0, 0.0),
+            end: Offset.zero,
+          ).animate(animation),
+          child: new SlideTransition(
 
-class TransSlideRoute<T> extends PageRouteBuilder<T> {
-  final Widget child;
-
-  TransSlideRoute({this.child})
-      : super(pageBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          return child;
-        }, transitionsBuilder: (BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child) {
-          return new RotationTransition(
-            turns: animation,
-            child: new ScaleTransition(
-              scale: animation,
-              child: new FadeTransition(
-                opacity: animation,
-                child: child,
+            position: new Tween<Offset>(
+              begin: Offset.zero,
+              end: const Offset(-1.0, 0.0),
+            ).animate(secondaryAnimation),
+            child: child,
+          ),
+        );
+        break;
+      case 'scale':
+        return ScaleTransition(
+          scale: new Tween<double>(
+            begin: 0.0,
+            end: 1.0,
+          ).animate(
+            CurvedAnimation(
+              parent: animation,
+              curve: Interval(
+                0.00,
+                0.50,
+                curve: curve,
               ),
             ),
-          );
-        });
-}
-
-class RightSlideRoute extends PageRouteBuilder {
-  final Widget child;
-
-  RightSlideRoute({this.child})
-      : super(
-          pageBuilder: (BuildContext context, Animation<double> animation,
-              Animation<double> secondaryAnimation) {
-            return child;
-          },
-          transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) {
-            return SlideTransition(
-              position: Tween<Offset>(
-                begin: const Offset(-1.0, 0.0),
-                end: Offset.zero,
-              ).animate(animation),
-              child: child,
-            );
-          },
-        );
-}
-
-class LeftSlideRoute extends PageRouteBuilder {
-  final Widget child;
-
-  LeftSlideRoute({this.child})
-      : super(pageBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          return child;
-        }, transitionsBuilder: (BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child) {
-          return SlideTransition(
-            position: new Tween<Offset>(
-              begin: const Offset(1.0, 0.0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: new SlideTransition(
-              position: new Tween<Offset>(
-                begin: Offset.zero,
-                end: const Offset(1.0, 0.0),
-              ).animate(secondaryAnimation),
-              child: child,
-            ),
-          );
-        });
-}
-
-class ScaleSlideRoute extends PageRouteBuilder {
-  final Widget screen;
-
-  ScaleSlideRoute({this.screen})
-      : super(pageBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          return screen;
-        }, transitionsBuilder: (BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child) {
-          return new ScaleTransition(
-            scale: new Tween<double>(
-              begin: 0.0,
+          ),
+          child: ScaleTransition(
+            scale: Tween<double>(
+              begin: 1.5,
               end: 1.0,
             ).animate(
               CurvedAnimation(
                 parent: animation,
                 curve: Interval(
-                  0.00,
                   0.50,
-                  curve: Curves.linear,
+                  1.00,
+                  curve:  curve,
                 ),
               ),
             ),
-            child: ScaleTransition(
-              scale: Tween<double>(
-                begin: 1.5,
-                end: 1.0,
-              ).animate(
-                CurvedAnimation(
-                  parent: animation,
-                  curve: Interval(
-                    0.50,
-                    1.00,
-                    curve: Curves.linear,
-                  ),
-                ),
-              ),
+            child: child,
+          ),
+        );
+        break;
+      case 'transform':
+        return new RotationTransition(
+          turns: animation,
+          child: new ScaleTransition(
+            scale: animation,
+            child: new FadeTransition(
+              opacity: animation,
               child: child,
             ),
-          );
-        });
+          ),
+        );
+        break;
+      case 'size' :
+        return Align(
+          child: SizeTransition(
+            sizeFactor:
+            CurvedAnimation(
+              parent: animation,
+              curve: curve,
+            ),
+            child: child,),
+        );
+        break;
+      default:
+        return FadeTransition(opacity: animation, child: child);
+    }
+  });
 }
